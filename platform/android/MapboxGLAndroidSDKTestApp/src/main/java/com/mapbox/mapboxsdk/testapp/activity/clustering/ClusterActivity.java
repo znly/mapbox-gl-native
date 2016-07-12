be9180a5aa35b001a11a5a851b44ef560bc1be42
Tobrun <tobrun.van.nuland@gmail.com>
2016-07-12 14:25:37 +0200

    [android] - adding a parent cluster item

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/clustering/ClusterActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/clustering/ClusterActivity.java
@@ -1,18 +1,26 @@
 package com.mapbox.mapboxsdk.testapp.activity.clustering;
 
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.constants.MapboxConstants;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Projection;
 import com.mapbox.mapboxsdk.testapp.R;
 
 public class ClusterActivity extends AppCompatActivity implements MapboxMap.OnCameraChangeListener {
@@ -20,6 +28,9 @@ public class ClusterActivity extends AppCompatActivity implements MapboxMap.OnCa
     private MapView mMapView;
     private MapboxMap mMapboxMap;
     private boolean mCollapsedCluster;
+
+    private static final LatLng LAT_LNG = new LatLng(51.502615, 4.972326);
+    private TextView parentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +54,30 @@ public class ClusterActivity extends AppCompatActivity implements MapboxMap.OnCa
             public void onMapReady(@NonNull final MapboxMap mapboxMap) {
                 mMapboxMap = mapboxMap;
                 setupMap();
+
+                final Projection projection = mapboxMap.getProjection();
+                mMapView.addOnMapChangedListener(new MapView.OnMapChangedListener() {
+
+                    @Override
+                    public void onMapChanged(@MapView.MapChange int change) {
+                        if (change == MapView.REGION_IS_CHANGING || change == MapView.REGION_DID_CHANGE || change == MapView.DID_FINISH_LOADING_MAP) {
+                            PointF point = projection.toScreenLocation(LAT_LNG);
+                            parentTextView.setX(point.x);
+                            parentTextView.setY(point.y);
+                        }
+                    }
+                });
             }
         });
+
+        parentTextView = new TextView(this);
+        parentTextView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        parentTextView.setText("3");
+        parentTextView.setTextColor(Color.WHITE);
+        parentTextView.setGravity(Gravity.CENTER);
+        parentTextView.setBackgroundColor(Color.RED);
+        parentTextView.setPadding(32, 32, 32, 32);
+        mMapView.addView(parentTextView);
     }
 
     private void setupMap() {
